5a7065d3e37c3011c7928b34ad2085847982c0ce
David Lange <lange@finiam.com>
2023-01-17 16:39:33 +0000

    add forms, fix number parsing to uint256

--- src/lib/components/Approve.svelte
+++ src/lib/components/Approve.svelte
@@ -1,22 +1,25 @@
 <script lang="ts">
   import accountStore from "$lib/svark/accountStore";
-  import { number } from "starknet";
-  import { parseInputAmountToUint256 } from "$lib/utils/parseInputAmountToUint256";
+  import { number, uint256 } from "starknet";
   import contractsStore from "$lib/svark/contractsStore";
+
+  let lusdAmount = 0;
+  let yearnAmount = 0;
 
   const lusdCt = $contractsStore.LUSD;
   const yearnCt = $contractsStore.yearnLUSD;
 
   async function approveLUSD() {
     const spender = number.toFelt($accountStore.address);
-    const amount = parseInputAmountToUint256("0");
+    const amount = uint256.bnToUint256(lusdAmount.toString());
 
     $lusdCt.approve(spender, amount);
   }
 
   async function approveYearnLUSD() {
     const spender = number.toFelt($accountStore.address);
-    const amount = parseInputAmountToUint256("0");
+    const amount = uint256.bnToUint256(yearnAmount.toString());
+    console.log(amount);
 
     $yearnCt.approve(spender, amount);
   }
@@ -26,5 +29,17 @@
   To begin, you'll have to authorize the smart contracts to manage your LUSD,
   and yearn LUSD. You can specify how much LUSD is controlled by the contract.
 </p>
-<button on:click={approveLUSD}>Approve LUSD</button>
-<button on:click={approveYearnLUSD}>Approve yearn LUSD</button>
+<div>
+  <p>LUSD</p>
+  <form on:submit|preventDefault={approveLUSD}>
+    <input type="number" name="amount" bind:value={lusdAmount} />
+    <button type="submit">Approve</button>
+  </form>
+</div>
+<div>
+  <p>Yearn LUSD</p>
+  <form on:submit|preventDefault={approveYearnLUSD}>
+    <input type="number" name="amount" bind:value={yearnAmount} />
+    <button type="submit">Approve</button>
+  </form>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import "../app.css";
+</script>
+
+<main>
+  <slot />
+</main>
+
+<style>
+  main {
+    margin: 2rem;
+  }
+</style>
