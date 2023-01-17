21268e5cecb4f4dd4b9baf7c15386c114ac640c3
David Lange <lange@finiam.com>
2023-01-17 16:21:50 +0000

    init contracts, add approve options

---
+++ src/lib/components/Approve.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import accountStore from "$lib/svark/accountStore";
+  import { number } from "starknet";
+  import { parseInputAmountToUint256 } from "$lib/utils/parseInputAmountToUint256";
+  import contractsStore from "$lib/svark/contractsStore";
+
+  const lusdCt = $contractsStore.LUSD;
+  const yearnCt = $contractsStore.yearnLUSD;
+
+  async function approveLUSD() {
+    const spender = number.toFelt($accountStore.address);
+    const amount = parseInputAmountToUint256("0");
+
+    $lusdCt.approve(spender, amount);
+  }
+
+  async function approveYearnLUSD() {
+    const spender = number.toFelt($accountStore.address);
+    const amount = parseInputAmountToUint256("0");
+
+    $yearnCt.approve(spender, amount);
+  }
+</script>
+
+<p>
+  To begin, you'll have to authorize the smart contracts to manage your LUSD,
+  and yearn LUSD. You can specify how much LUSD is controlled by the contract.
+</p>
+<button on:click={approveLUSD}>Approve LUSD</button>
+<button on:click={approveYearnLUSD}>Approve yearn LUSD</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,37 @@
 <script lang="ts">
-	import contractStore from '$lib/svark/contract';
-	import accountStore from '$lib/svark/accountStore';
-	import connectStore from '$lib/svark/connectStore';
-	import LUSD_ABI from '$lib/data/LUSD_ABI.json';
+  import contractStore from "$lib/svark/contract";
+  import accountStore from "$lib/svark/accountStore";
+  import connectStore from "$lib/svark/connectStore";
+  import LUSD_ABI from "$lib/data/LUSD_ABI.json";
+  import YEARN_ABI from "$lib/data/YEARN_ABI.json";
+  import Approve from "$lib/components/Approve.svelte";
 
-	contractStore('LUSD', {
-		contractAddress: import.meta.env.VITE_CONTRACT_ADDRESS,
-		abi: LUSD_ABI,
-		providerOrAccount: $accountStore.account
-	});
+  async function connect() {
+    await connectStore.connectWallet();
+
+    contractStore("LUSD", {
+      contractAddress: import.meta.env.VITE_LUSD_CONTRACT,
+      abi: LUSD_ABI,
+      providerOrAccount: $accountStore.account
+    });
+    contractStore("yearnLUSD", {
+      contractAddress: import.meta.env.VITE_YEARN_CONTRACT,
+      abi: YEARN_ABI,
+      providerOrAccount: $accountStore.account
+    });
+  }
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<p>{$accountStore.address}</p>
+<h1>Chicken bonds</h1>
 
 {#if !$accountStore.connected}
-	<button on:click={() => connectStore.connectWallet()}>Connect</button>
+  <button on:click={connect}>Connect</button>
+{:else}
+  <p>
+    Hello, {$accountStore.address.substring(
+      0,
+      4
+    )}...{$accountStore.address.slice(-5)}
+  </p>
+  <Approve />
 {/if}
